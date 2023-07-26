35ae823d0051021c9be9738779c2150ffbbec15a
Gabriel-Perugini <123771750+Gabriel-Perugini@users.noreply.github.com>
2023-07-26 13:51:36 -0300

    Add files via upload

---
+++ Primeira aula JS.js
@@ -0,0 +1,69 @@
+// [1] Print da idade abaixo
+console.log("16 anos")
+
+// [2] Print das semanas progamando nas ferias abaixo
+console.log("Progamei cerca de 1 semana nas ferias")
+
+/*
+[3]
+I
+S
+S
+O
+?
+*/
+
+let monk = "macaco" // [4] Variavel "macaco"
+
+let num = 1 // [4] Variavel "numero pequeno"
+
+let verdade = true // [4] Variavel booleana chamada verdade
+
+let nulo = null // [4] Variavel nula/vazia
+
+let baba = undefined // [4] Variavel Indefinida
+
+// [5] Print da idade mais 3.5
+console.log(16 + 3.5)
+
+// [6] Print de a qnt anos pousaram na lua
+console.log(2023 - 1969, "anos atrás")
+
+// [7] Divisão de 65 por 240
+console.log(65 / 240)
+
+// [8] Multiplicação de 0.2708 por 100
+console.log(0.2708 * 100)
+
+// [9] Concatenação sem espaço
+console.log("Hello" +  "world")
+
+// [10] Concatenação com espaço
+console.log("Hello " + "world")
+
+// [11] Length
+var monkey = 'Macaco';
+var vazio = '';
+
+/* "Mozilla possui 7 unidades de código de comprimento" */
+console.log('Macaco possui ' + monkey.length + ' unidades de código de comprimento');
+
+/* "A string vazia possui um comprimento de 0" */
+console.log('A string vazia possui um comprimento de ' + vazio.length);
+
+// [12] upper
+let k = "Kaian Novais"
+console.log(k.toUpperCase())
+
+// [13] sem espaços
+let l = "             Professor Kaian Novais    "
+console.log(l.trim())
+
+// [14] multiplicação aleatoria
+console.log(100 * Math.random())
+
+// [15] arredondar pra cima
+console.log(Math.ceil(43.8))
+
+// [16] é numero inteiro?
+console.log(Number.isInteger(2017))
